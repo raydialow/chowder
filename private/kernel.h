4c0d70e78ec2b54c5febc5986c7c24f6a2bee440
June Rana <jsrana@raydialow.xyz>
2020-09-26 21:55:09 -0700

    considering bundling SDL2...

--- private/kernel.h
+++ private/kernel.h
@@ -1,5 +1,5 @@
 /*
- *  kernel.c --- the Chowder game engine kernel
+ *  kernel.h --- the Chowder game engine kernel header
  *
  *  Copyright 2020 June Sage Rana
  *
@@ -18,19 +18,79 @@
  *
  */
 
+#include <assert.h>
+#include <stdbool.h>
 #include <vulkan/vulkan.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_video.h>
+#include <SDL/SDL_vulkan.h>
 
 const uint32_t chowder_version = VK_MAKE_VERSION(0, 0, 0);
 
-VkApplicationInfo make_application_info(uint32_t maj, uint32_t min, uint32_t rev)
+VkApplicationInfo mk_application_info(uint32_t application_version)
 {
   VkApplicationInfo ret;
   ret.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
   ret.pNext = NULL;
   ret.pApplicationName = "Unnamed";
-  ret.applicationVersion = VK_MAKE_VERSION(maj, min, rev);
+  ret.applicationVersion = application_version;
   ret.pEngineName = "Chowder";
   ret.engineVersion = chowder_version;
   ret.apiVersion = VK_API_VERSION_1_2;
   return ret;
+}
+
+SDL_Window* mk_window(uint32_t width, uint32_t height, bool fullscreen)
+{
+  SDL_Init(SDL_INIT_EVERYTHING);
+  uint32_t flags;
+  if(fullscreen == true) {
+	flags = SDL_WINDOW_VULKAN|SDL_WINDOW_FULLSCREEN;
+  } else {
+	flags = SDL_WINDOW_VULKAN;
+  }
+  return SDL_CreateWindow("Chowder", 0, 0, width, height, flags);
+}
+
+VkInstanceCreateInfo mk_instance_create_info(VkApplicationInfo application_info,
+											 SDL_Window* window,
+											 bool validation_layers)
+{
+  VkInstanceCreateInfo ret;
+
+  uint32_t extension_count;
+  SDL_Vulkan_GetInstanceExtensions(window, &extension_count, NULL);
+  char** extension_names;
+  SDL_Vulkan_GetInstanceExtensions(window, &extension_count, extension_names);
+
+  uint32_t vlayers_count = 0;
+  char ** vlayers_names = NULL;
+  if(validation_layers == true) {
+	//do something else
+  }
+
+  ret.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  ret.pNext = NULL;
+  ret.flags = 0;
+  ret.pApplicationInfo = application_info;
+  ret.enabledLayerCount = vlayers_count;
+  ret.ppEnabledLayerNames = vlayers_names;
+  ret.enabledExtensionCount = extension_count;
+  ret.ppEnabledExtensionNames = extension_names;
+
+  return ret;
+}
+
+VkInstance mk-instance(VkInstanceCreateInfo instance_create_info)
+{
+  VkInstance ret;
+  assert(vkCreateInstance(instance_create_info, NULL, &ret) == VK_SUCCESS);
+  return ret;
+}
+
+void rm-instance(VkInstance instance, SDL_Window* window)
+{
+  vkDestroyInstance(instance, NULL);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 }
