130559db93c93e2b72c49833504fc6d8a832200d
June Rana <jsrana@raydialow.xyz>
2020-10-25 00:32:57 -0700

    cleaner

--- c/kernel.c
+++ c/kernel.c
@@ -29,38 +29,136 @@
 #include <GLFW/glfw3.h>
 #endif
 
-#include "kernel.h"
+/*
+ * Typedefs
+ */
+typedef uint32_t vkint;
 
-struct chrQueues {
-  VkQueue q2D; // 2D queue for 2d fx, particles, ui, etc.
-  VkQueue qTC; // TraceCollision queue for collisions and ray tracing comps, etc.
-  VkQueue q3D; // 3D queue for geometry ops, tesselation, etc.
-  VkQueue qRP; // RenderPipeline queue for shaders, raster, render, etc.
-} chrQueues;
+/*
+ * Constants
+ */
+const char* chowder_name        = "Chowder v0.0.0";
+const vkint chowder_version     = VK_MAKE_VERSION(0, 0, 0);
+const char* chowder_sdk_name    = "Chowder SDK v0.0.0";
+const vkint chowder_sdk_version = VK_MAKE_VERSION(0, 0, 0);
 
+const float queue_priorities[4] = {.5,.5,.5,.5};
+
+/*
+ * MAIN
+ */
 int main()
 {
-  // start up
-  const char* application_name = "Unnamed Application";
-  const vkint application_version = VK_MAKE_VERSION(0, 0, 0);
-  VkApplicationInfo application_info = mk_application_info(application_name, application_version);
-  vkint width = 800;
-  vkint height = 600;
-  const char* window_title = "Chowder v0.0.0";
+    /*
+     * Declaration
+     */
+    VkApplicationInfo               application_info;
+    GLFWwindow*                     window;
+    vkint                           extension_count;
+    const char**                    extension_names;
+    VkInstanceCreateInfo            instance_create_info;
+    VkInstance                      instance;
+    vkint                           physical_device_count;
+    VkPhysicalDevice                physical_device;
+    VkPhysicalDeviceProperties      physical_device_properties;
+    VkPhysicalDeviceFeatures        physical_device_feats;
+    vkint                           queue_family_count;
+    VkDeviceQueueCreateInfo         device_queue_create_info[1];
+    bool                            queue_family_set;
+    vkint                           queue_family_index;
+    VkDeviceCreateInfo              device_create_info;
+    VkDevice                        device;
+    /* Device Queues */
+    //VkQueue                       queue_2d;
+    //VkQueue                       queue_3d;
+    //VkQueue                       queue_tc;
+    //VkQueue                       queue_rp;
 
-  puts("Making window.");
-  GLFWwindow* window = mk_window(width, height, window_title);
-  bool validation_layers = false;
-  VkInstanceCreateInfo instance_create_info;
+    /*
+     * Initialization
+     */
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); //using Vulkan
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-  puts("Making instance create info.");
-  instance_create_info = mk_instance_create_info(application_info, validation_layers);
+    application_info.sType                          = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    application_info.pNext                          = NULL;
+    application_info.pApplicationName               = chowder_sdk_name;
+    application_info.applicationVersion             = chowder_sdk_version;
+    application_info.pEngineName                    = chowder_name;
+    application_info.engineVersion                  = chowder_version;
+    application_info.apiVersion                     = VK_API_VERSION_1_2;
 
-  puts("Making instance.");
-  VkInstance instance = mk_instance(&instance_create_info);
+    window                                          = glfwCreateWindow(800, 600, chowder_sdk_name, NULL, NULL);
+    extension_count                                 = 0;
+    extension_names                                 = glfwGetRequiredInstanceExtensions(&extension_count);
 
-  // shut down
-  puts("Shutting down.");
-  chr_shutdown(instance, window);
-  return 0;
+    instance_create_info.sType                      = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instance_create_info.pNext                      = NULL;
+    instance_create_info.flags                      = 0;
+    instance_create_info.pApplicationInfo           = &application_info;
+    instance_create_info.enabledLayerCount          = 0; // vlayers
+    instance_create_info.ppEnabledLayerNames        = NULL; // vlayers
+    instance_create_info.enabledExtensionCount      = extension_count;
+    instance_create_info.ppEnabledExtensionNames    = extension_names;
+
+    assert( vkCreateInstance(&instance_create_info, NULL, &instance) == VK_SUCCESS );
+    assert( vkEnumeratePhysicalDevices(instance, &physical_device_count, NULL) == VK_SUCCESS );
+    assert( physical_device_count > 0 );
+    VkPhysicalDevice physical_devices[physical_device_count];
+    assert( vkEnumeratePhysicalDevices(instance, &physical_device_count, physical_devices) == VK_SUCCESS );
+
+    /* Grabbing First Device by Default */
+    physical_device                                 = physical_devices[0];
+
+    vkGetPhysicalDeviceProperties(physical_device, &physical_device_properties); //void
+    vkGetPhysicalDeviceFeatures(physical_device, &physical_device_feats); //void
+    vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &queue_family_count, NULL); //void
+    assert( queue_family_count > 0 );
+    VkQueueFamilyProperties queue_family_props[queue_family_count];
+    vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &queue_family_count, &queue_family_props[0]); //void
+
+    /* Assure Queue Family has graphics, compute, and transfer bits set and can hold at least 4 queues. */
+    queue_family_set = false;
+    for(vkint iter = 0; iter < queue_family_count; iter += 1) {
+        vkint queue_flags = queue_family_props[iter].queueFlags;
+        vkint queue_count = queue_family_props[iter].queueCount;
+        if(queue_flags&VK_QUEUE_GRAPHICS_BIT
+           && queue_flags&VK_QUEUE_COMPUTE_BIT
+           && queue_flags&VK_QUEUE_TRANSFER_BIT
+           && queue_count >= 4) {
+            queue_family_index                      = iter;
+            queue_family_set                        = true;
+            break;
+        }
+    }
+    assert( queue_family_set );
+
+    device_queue_create_info[0].sType               = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    device_queue_create_info[0].pNext               = NULL;
+    device_queue_create_info[0].flags               = 0;
+    device_queue_create_info[0].queueFamilyIndex    = queue_family_index; // temporary assignment
+    device_queue_create_info[0].queueCount          = 4;
+    device_queue_create_info[0].pQueuePriorities    = queue_priorities;
+
+    device_create_info.sType                        = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    device_create_info.pNext                        = NULL;
+    device_create_info.flags                        = 0;
+    device_create_info.queueCreateInfoCount         = 1;
+    device_create_info.pQueueCreateInfos            = device_queue_create_info;
+    device_create_info.enabledLayerCount            = 0;
+    device_create_info.ppEnabledLayerNames          = NULL;
+    device_create_info.enabledExtensionCount        = 0;
+    device_create_info.ppEnabledExtensionNames      = NULL;
+    device_create_info.pEnabledFeatures             = &physical_device_feats;
+
+    assert( vkCreateDevice(physical_device, &device_create_info, NULL, &device) == VK_SUCCESS );
+
+    /*
+     * Procedure
+     */
+    vkDestroyInstance(instance, NULL);
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 666;
 }
